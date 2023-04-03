a90281aaccc7a6d84c3e5a0fb4302752820a00a5
locia <locia@git.com>
2023-04-03 21:32:43 +0800

    项目初始化

---
+++ src/main/java/com/bjpn/Demo01.java
@@ -0,0 +1,12 @@
+package com.bjpn;
+
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
+public class Demo01 {
+    private int id;
+    private String name;
+    private String address;
+}
