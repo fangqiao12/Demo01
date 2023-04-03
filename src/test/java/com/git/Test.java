2d105ed602ffd71df1f986877bc44f29db1c2db3
locia <locia@git.com>
2023-04-03 21:44:02 +0800

    修改Test.java文件

--- src/test/java/com/git/Test.java
+++ src/test/java/com/git/Test.java
@@ -3,5 +3,8 @@ package com.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test01...");
+        System.out.println("test02...");
+        System.out.println("test03...");
+        System.out.println("test04...");
     }
 }
