54eb46580bad8013288175e023e89f840cc87269
locia <locia@git.com>
2023-04-04 17:45:43 +0800

    C6客户端更新操作

--- src/test/java/com/git/Test02.java
+++ src/test/java/com/git/Test02.java
@@ -3,6 +3,7 @@ package com.git;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("test02...");
-        System.out.println("C5客户端->test02...");
+        System.out.println("C6客户端->test02...");
+
     }
 }
